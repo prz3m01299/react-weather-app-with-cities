5181763355ac9a5fda68eb970c69b710c2330872
Prz3m01299 <Prz3m01299@gmailcom>
2023-06-05 17:23:08 +0100

    Added functions like search, utilized cities API and temperature API

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '035e51bfccmshed68976c6e18460p1f4906jsneedb4302e041',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
